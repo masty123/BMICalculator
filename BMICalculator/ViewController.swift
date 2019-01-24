5a97b58424e532cff0ff895e60dd9afe98347e16
masty123 <theeruth.b@ku.th>
2019-01-24 14:37:48 +0700

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Theeruth Borisuth on 24/1/19.
+//  Copyright © 2019 Theeruth Borisuth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BMICalculatorTests/BMICalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  BMICalculatorTests.swift
+//  BMICalculatorTests
+//
+//  Created by Theeruth Borisuth on 24/1/19.
+//  Copyright © 2019 Theeruth Borisuth. All rights reserved.
+//
+
+import XCTest
+@testable import BMICalculator
+
+class BMICalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
